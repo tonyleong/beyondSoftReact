a1653ac18935799b7405daa9734f47745d916a48
Tony <59461665+tonyleong@users.noreply.github.com>
2023-02-18 00:19:05 +0800

    cosmetic and code clean

--- src/component/searchbar/index.jsx
+++ src/component/searchbar/index.jsx
@@ -3,6 +3,7 @@ import Autocomplete from '@mui/material/Autocomplete'
 import TextField from '@mui/material/TextField';
 import { useDispatch, useSelector } from 'react-redux';
 import { search, setPosition } from '../../redux/mapSlice'
+import './style.css'
 
 const Searchbar = () => {
     const [searchInput, setSearchInput] = useState('')
@@ -26,13 +27,17 @@ const Searchbar = () => {
         dispatch(setPosition(value))
     }
 
-    return <Autocomplete
-        options={searchResult}
-        onInput={handleSearch}
-        onChange={handleOnChange}
-        getOptionLabel={opt => opt?.name}
-        renderInput={(params) => <TextField {...params} label="Address" />}
-    />
+    return (
+        <div className='searchContainer'>
+            <Autocomplete
+                size='small'
+                options={searchResult}
+                onInput={handleSearch}
+                onChange={handleOnChange}
+                getOptionLabel={opt => opt?.name}
+                renderInput={(params) => <TextField {...params} label="Address" />}
+            />
+        </div>)
 }
 
 export { Searchbar as default, Searchbar }
--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,7 @@ import App from './App'
 import { store } from './store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-
-  // </React.StrictMode>,
+  <Provider store={store}>
+    <App />
+  </Provider>
 )
